d7609304ba2efea5a7a2ec6cf195941529b48126
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-12 19:18:48 -0400

    send message function

--- src/components/CurrentMessage.js
+++ src/components/CurrentMessage.js
@@ -1,19 +1,64 @@
 import { ethers } from 'ethers';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MessageCard from './MessageCard'
 import {DECEN_MESSAGE_CONTRACT} from "../contractsConfig"
 import decenmessagingjson from "../assets/decenmessaging.json"
 
 const CurrentMessage = () => {
+  const [currentAccount, setCurrentAccount] = useState()
 
-  const returnCurrentMessage = async () =>{
+  const [allMessages, setAllMessages] = useState()
+  const [currentMessage, setCurrentMessage] = useState()
+  const [replyMessage, setReplyMessage] = useState()
+  const [isReplying, setIsReplying] = useState(false)
+  const [replyAddress, setReplyAddress] = useState()
+
+  const checkIfWalletIsConnected = async () =>{
+    const {ethereum} = window;
+    if(!ethereum){
+        alert("Please download MetaMask")
+        return;
+    }else{
+        console.log("Ethereum Object Found")
+    }
+
+    const accounts = await ethereum.request({method: "eth_accounts"})
+
+    if(accounts.length !== 0){
+        setCurrentAccount(accounts[0])
+
+        console.log(`Current Account ${accounts[0]}`)
+
+    }else{
+        console.log("no authorized account found")
+    }
+    
+    
+
+}
+
+  const returnAllMessages = async () =>{
     try{
       const {ethereum} = window;
       if(ethereum){
         const provider = new ethers.providers.Web3Provider(ethereum)
         const DecenMessaging = new ethers.Contract(DECEN_MESSAGE_CONTRACT, decenmessagingjson.abi, provider )
 
-        const messages = await DecenMessaging.viewReceivedMessages()
+        const messages = await DecenMessaging.viewReceivedMessages(currentAccount)
+
+        const filteredData = messages.map((i)=>{
+          let output = []
+          output.push(i[0].toString()) // message number
+          output.push(i[1]) //sender
+          output.push(i[2]) // receiver
+          output.push(i[3].toString()) // timestamp
+          output.push(i[4].toString()) // message text
+          return output
+        })
+
+        setAllMessages(filteredData[0]) // hard coding the selected message
+        setReplyAddress(filteredData[0][1])
+        
       }
 
     }catch(error){
@@ -21,13 +66,65 @@ const CurrentMessage = () => {
     }
   }
 
+  const sendReplyMessage = async () =>{
+    try{
+      const {ethereum} = window;
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const DecenMessagingContract = new ethers.Contract(DECEN_MESSAGE_CONTRACT, decenmessagingjson.abi, signer)
+
+        let txn = await DecenMessagingContract.sendMessage(replyAddress, replyMessage)
+        let receipt = await txn.wait()
+
+        if(receipt.status === 1){
+          console.log("Message Sent Successful!")
+          
+
+        } else {
+          alert("Transaction failed, please try again")
+        }
+      }
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    checkIfWalletIsConnected()
+    
+  },[])
+  
+  useEffect(()=>{
+    returnAllMessages()
+  },[currentAccount])
+
+ 
 
   return (
-    <div id="content" className='message-container'>
-        <MessageCard 
-          from="test"
-          messagetext="a long paragraph of text, super long lorem ipsom keeping the texting going, hopefully will see a wrap around shortly"
-        />
+    <div id="from-header-container">
+    {!allMessages ? <p>loading</p> :
+      (
+        <>
+        <h2 >{allMessages[1]}</h2>
+        <div>
+            <p>{allMessages[4]} </p>
+        </div>
+        <div>
+          {!isReplying ? <button onClick={setIsReplying(true)}>reply</button> :
+            <div>
+              <input type="textarea" onChange={e=>setReplyMessage(e.target.value)} />
+              <button onClick={sendReplyMessage} >Send</button>
+            </div>
+          }
+            
+            <button>Delete</button>
+        </div>
+        </>
+      )
+    }
+          
     </div>
   )
 }
--- src/components/RecentMessages.js
+++ src/components/RecentMessages.js
@@ -1,19 +1,101 @@
-import React from 'react'
+
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react'
+import MessageCard from './MessageCard'
+import {DECEN_MESSAGE_CONTRACT} from "../contractsConfig"
+import decenmessagingjson from "../assets/decenmessaging.json"
 import MessagePreviewCard from './MessagePreviewCard'
 
 const RecentMessages = () => {
+  const [currentAccount, setCurrentAccount] = useState()
+
+  const [allMessages, setAllMessages] = useState()
+
+  const checkIfWalletIsConnected = async () =>{
+    const {ethereum} = window;
+    if(!ethereum){
+        alert("Please download MetaMask")
+        return;
+    }else{
+        console.log("Ethereum Object Found")
+    }
+
+    const accounts = await ethereum.request({method: "eth_accounts"})
+
+    if(accounts.length !== 0){
+        setCurrentAccount(accounts[0])
+
+        console.log(`Current Account ${accounts[0]}`)
+
+    }else{
+        console.log("no authorized account found")
+    }
+    
+    
+
+}
+
+  const returnAllMessages = async () =>{
+    try{
+      const {ethereum} = window;
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const DecenMessaging = new ethers.Contract(DECEN_MESSAGE_CONTRACT, decenmessagingjson.abi, provider )
+
+        const messages = await DecenMessaging.viewReceivedMessages(currentAccount)
+
+        const filteredData = messages.map((i)=>{
+          let output = []
+          output.push(i[0].toString()) // message number
+          output.push(i[1]) //sender
+          output.push(i[2]) // receiver
+          output.push(i[3].toString()) // timestamp
+          output.push(i[4].toString()) // message text
+          return output
+        })
+
+        console.log(await messages)
+
+        setAllMessages(filteredData)
+        
+        
+      }
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    checkIfWalletIsConnected()
+    
+  },[currentAccount])
+
+  useEffect(()=>{
+    returnAllMessages()
+  },[currentAccount])
+
   return (
     <div id="content" className='recent-messages-container'>
         <div className='recent-messages-container2'>
             <ul className='messages-list'>
                 <h4 className='recent-messages-heading'>Recent Messages</h4>
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
-                <MessagePreviewCard from="test user 2 address" messagetext="long message text testing slice " />
+               
+                {
+                  !allMessages ? <p>no recent messages</p> :
+                  allMessages.map((i)=>{
+                    return(
+                    <div key={i[0]} className='message-preview-container'>
+                        <div className='message-preview'>
+                            <p >{i[1].slice(0,4)}...{i[1].slice(-6)}</p>
+                            <div>
+                                <p>{i[4].slice(0, 10)} </p>
+                            </div>
+                        </div>
+                    </div>)
+                  })
+                }
+                
 
                 
             </ul>
