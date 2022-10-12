7e8437e4625e947cd52cfd5a6e5b1275e06928a9
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-11 20:23:16 -0400

    seed messages

--- src/components/CurrentMessage.js
+++ src/components/CurrentMessage.js
@@ -1,7 +1,27 @@
+import { ethers } from 'ethers';
 import React from 'react'
 import MessageCard from './MessageCard'
+import {DECEN_MESSAGE_CONTRACT} from "../contractsConfig"
+import decenmessagingjson from "../assets/decenmessaging.json"
 
 const CurrentMessage = () => {
+
+  const returnCurrentMessage = async () =>{
+    try{
+      const {ethereum} = window;
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const DecenMessaging = new ethers.Contract(DECEN_MESSAGE_CONTRACT, decenmessagingjson.abi, provider )
+
+        const messages = await DecenMessaging.viewReceivedMessages()
+      }
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+
   return (
     <div id="content" className='message-container'>
         <MessageCard 
